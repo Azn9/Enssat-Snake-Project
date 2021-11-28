41629ad3496542e34f3fa38868077da36610c28e
Axel Joly <Azn9.pro@gmail.com>
2021-11-28 21:17:48 +0100

    WIP

--- Astar.c
+++ Astar.c
@@ -2,23 +2,23 @@
 
 void displayGrid();
 
-void computeCell(int x, int y, int sourcex, int sourcey, int destinationx, int destinationy);
+void computeCell(int x, int y, int sourcex, int sourcey, int destinationx, int destinationy, int previousCost);
 
-#define mapxsize 100
-#define mapysize 100
+#define mapxsize 10
+#define mapysize 10
 
 int distance(int source_x, int source_y, int destination_x, int destination_y);
 
 void tracePath(int sourcex, int sourcey, int destinationy, int destinationx);
 
-int weights[mapysize][mapxsize];
+int cost[mapysize][mapxsize];
 int calculated[mapysize][mapxsize];
 int chosen[mapysize][mapxsize];
 
 int main() {
     for (int y = 0; y < mapysize; y++) {
         for (int x = 0; x < mapxsize; x++) {
-            weights[y][x] = 0;
+            cost[y][x] = 0;
             calculated[y][x] = 0;
             chosen[y][x] = 0;
         }
@@ -28,42 +28,57 @@ int main() {
         calculated[y][0] = -1;
         calculated[y][mapxsize - 1] = -1;
 
-        weights[y][0] = -1;
-        weights[y][mapxsize - 1] = -1;
+        cost[y][0] = -1;
+        cost[y][mapxsize - 1] = -1;
     }
 
     for (int x = 0; x < mapxsize; x++) {
         calculated[0][x] = -1;
         calculated[mapysize - 1][x] = -1;
 
-        weights[0][x] = -1;
-        weights[mapysize - 1][x] = -1;
+        cost[0][x] = -1;
+        cost[mapysize - 1][x] = -1;
     }
 
     calculated[4][3] = -1;
-    weights[4][3] = -1;
+    cost[4][3] = -1;
 
     calculated[5][3] = -1;
-    weights[5][3] = -1;
+    cost[5][3] = -1;
 
     calculated[6][3] = -1;
-    weights[6][3] = -1;
+    cost[6][3] = -1;
 
     calculated[7][3] = -1;
-    weights[7][3] = -1;
+    cost[7][3] = -1;
 
     calculated[8][3] = -1;
-    weights[8][3] = -1;
+    cost[8][3] = -1;
 
     calculated[4][4] = -1;
-    weights[4][4] = -1;
+    cost[4][4] = -1;
 
     calculated[4][5] = -1;
-    weights[4][5] = -1;
+    cost[4][5] = -1;
 
-    computeCell(1, 1, 1, 1, 98, 98);
+    calculated[4][6] = -1;
+    cost[4][6] = -1;
 
-    tracePath(98, 98, 1, 1);
+    calculated[3][6] = -1;
+    cost[3][6] = -1;
+
+    calculated[2][6] = -1;
+    cost[2][6] = -1;
+
+    calculated[1][6] = -1;
+    cost[1][6] = -1;
+
+    computeCell(1, 1, 1, 1, 8, 8, 0);
+
+    calculated[1][1] = 1;
+    cost[1][1] = 0;
+
+    tracePath(8, 8, 1, 1);
 
     displayGrid();
 
@@ -81,7 +96,7 @@ void tracePath(int sourcex, int sourcey, int destinationy, int destinationx) {
     int currentWeight;
 
     if (sourcex > 0 && chosen[sourcey][sourcex - 1] == 0) {
-        currentWeight = weights[sourcey][sourcex - 1];
+        currentWeight = cost[sourcey][sourcex - 1];
 
         if (currentWeight >= 0 && currentWeight < lessweight) {
             lessweight = currentWeight;
@@ -91,7 +106,7 @@ void tracePath(int sourcex, int sourcey, int destinationy, int destinationx) {
     }
 
     if (sourcex < mapxsize - 1 && chosen[sourcey][sourcex + 1] == 0) {
-        currentWeight = weights[sourcey][sourcex + 1];
+        currentWeight = cost[sourcey][sourcex + 1];
 
         if (currentWeight >= 0 && currentWeight < lessweight) {
             lessweight = currentWeight;
@@ -101,7 +116,7 @@ void tracePath(int sourcex, int sourcey, int destinationy, int destinationx) {
     }
 
     if (sourcey > 0 && chosen[sourcey - 1][sourcex] == 0) {
-        currentWeight = weights[sourcey - 1][sourcex];
+        currentWeight = cost[sourcey - 1][sourcex];
 
         if (currentWeight >= 0 && currentWeight < lessweight) {
             lessweight = currentWeight;
@@ -111,10 +126,10 @@ void tracePath(int sourcex, int sourcey, int destinationy, int destinationx) {
     }
 
     if (sourcex < mapysize - 1 && chosen[sourcey + 1][sourcex] == 0) {
-        currentWeight = weights[sourcey + 1][sourcex];
+        currentWeight = cost[sourcey + 1][sourcex];
 
         if (currentWeight >= 0 && currentWeight < lessweight) {
-            lessweight = currentWeight;
+//            lessweight = currentWeight;
             lessx = sourcex;
             lessy = sourcey + 1;
         }
@@ -129,46 +144,46 @@ void tracePath(int sourcex, int sourcey, int destinationy, int destinationx) {
 
 int calculWeight(int x, int y, int sourcex, int sourcey, int destinationx, int destinationy) {
     if (calculated[y][x] != 0) {
-        return weights[y][x];
+        return cost[y][x];
     }
 
-    int wstart = distance(x, y, sourcex, sourcey);
+//    int wstart = distance(x, y, sourcex, sourcey);
     int wend = distance(x, y, destinationx, destinationy);
 
-    if (wstart > wend) {
-        return 2 * wend + wstart;
-    } else {
-        return 2 * wstart + wend;
-    }
+    return wend;
 }
 
-void computeCell(int x, int y, int sourcex, int sourcey, int destinationx, int destinationy) {
+void computeCell(int x, int y, int sourcex, int sourcey, int destinationx, int destinationy, int previousCost) {
     if (calculated[y][x] != 0) {
         printf("\033[0;32mCalculated !\033[0m\n");
         return;
     }
 
-    if (weights[destinationy][destinationx] != 0) {
+    if (cost[destinationy][destinationx] != 0) {
         printf("\033[0;32mWeight found !\033[0m\n");
         return;
     }
 
-    weights[y][x] = calculWeight(x, y, sourcex, sourcey, destinationx, destinationy);
+    int currentCost = cost[y][x];
 
-    if (x > 0) {
-        weights[y][x - 1] = calculWeight(x - 1, y, sourcex, sourcey, destinationx, destinationy);
+    if (x > 0 && cost[y][x - 1] == 0) {
+        cost[y][x - 1] = currentCost + 1;
+//        costHeuristic[y][x - 1] = currentCost + 1 + calculWeight(x - 1, y, sourcex, sourcey, destinationx, destinationy);
     }
 
-    if (y > 0) {
-        weights[y - 1][x] = calculWeight(x, y - 1, sourcex, sourcey, destinationx, destinationy);
+    if (y > 0 && cost[y - 1][x] == 0) {
+        cost[y - 1][x] = currentCost + 1;
+//        costHeuristic[y - 1][x] = currentCost + 1 + calculWeight(x, y - 1, sourcex, sourcey, destinationx, destinationy);
     }
 
-    if (x < mapxsize - 1) {
-        weights[y][x + 1] = calculWeight(x + 1, y, sourcex, sourcey, destinationx, destinationy);
+    if (x < mapxsize - 1 && cost[y][x + 1] == 0) {
+        cost[y][x + 1] = currentCost + 1;
+//        costHeuristic[y][x + 1] = currentCost + 1 + calculWeight(x + 1, y, sourcex, sourcey, destinationx, destinationy);
     }
 
-    if (y < mapysize - 1) {
-        weights[y + 1][x] = calculWeight(x, y + 1, sourcex, sourcey, destinationx, destinationy);
+    if (y < mapysize - 1 && cost[y + 1][x] == 0) {
+        cost[y + 1][x] = currentCost + 1;
+//        costHeuristic[y + 1][x] = currentCost + 1 + calculWeight(x, y + 1, sourcex, sourcey, destinationx, destinationy);
     }
 
     displayGrid();
@@ -185,7 +200,7 @@ void computeCell(int x, int y, int sourcex, int sourcey, int destinationx, int d
                 continue;
             }
 
-            int currentWeight = weights[testy][testx];
+            int currentWeight = cost[testy][testx];
 
             if (currentWeight > 0) {
                 if (currentWeight < lessweight) {
@@ -197,11 +212,11 @@ void computeCell(int x, int y, int sourcex, int sourcey, int destinationx, int d
         }
     }
 
-    computeCell(lessx, lessy, sourcex, sourcey, destinationx, destinationy);
+    computeCell(lessx, lessy, sourcex, sourcey, destinationx, destinationy, currentCost);
 }
 
 char *color(int x, int y) {
-    switch (weights[y][x]) {
+    switch (cost[y][x]) {
         case 0:
             return "\033[0m";
         case -1:
@@ -217,7 +232,7 @@ char *color(int x, int y) {
 void displayGrid() {
     for (int y = 0; y < mapysize; y++) {
         for (int x = 0; x < mapysize; x++) {
-            printf("%s %3d \033[0m", color(x, y), weights[y][x]);
+            printf("%s %3d \033[0m", color(x, y), cost[y][x]);
         }
         printf("\n");
     }
